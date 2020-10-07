df12af0ede88d3f7240ffb79a7d1c8a4d322673d
pvpatil5 <pvpatil123@yahoo.com>
2020-10-07 17:01:09 +0530

    Commit 1

---
+++ src/pac/Swapping.java
@@ -0,0 +1,16 @@
+package pac;
+
+public class Swapping {
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 12;
+		
+		x= x +y;
+		y = x-y;
+		x = x-y;
+		
+		System.out.println("x Value is :" + x);
+		System.out.println("y value is :" + y);
+		
+}
+}
